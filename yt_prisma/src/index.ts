93af35059eac1195ac717dc2721163e1822ef5e5
SAMBEET SEKHAR PANDA <sambeetpanda507@gmail.com>
2022-12-27 19:43:54 +0530

    Created tables

--- yt_prisma/src/index.ts
+++ yt_prisma/src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello World!!!");
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient({ log: ['query'] });
+
+const main = async () => {
+  try {
+    const users = await prisma.user.findMany({
+      include: {
+        preferences: true,
+      },
+    });
+
+    console.log('users: ', users);
+  } catch (error: any) {
+    console.log(error.message);
+  } finally {
+    console.log('Disconnecting...');
+    await prisma.$disconnect();
+  }
+};
+
+main();
